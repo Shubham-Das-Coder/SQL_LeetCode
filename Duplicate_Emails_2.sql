cde7d7079b17135367ee0aa4f798cd3fb0b68d30
Shubham Das <shubhamdaskgp@gmail.com>
2023-10-18 20:09:28 +0530

    Add files via upload

---
+++ Duplicate_Emails_2.sql
@@ -0,0 +1,5 @@
+/*
+Link to the question : https://leetcode.com/problems/duplicate-emails/description/
+Leetcode question number : 182
+*/
+select email from person group by email having count(distinct id)>1;
