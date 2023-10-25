2faeb772cadfee8b69417cb32973f50cc5ca979d
Shubham Das <shubhamdaskgp@gmail.com>
2023-10-25 14:41:12 +0530

    Update Patients_With_A_Condition_1.sql

--- Patients_With_A_Condition_1.sql
+++ Patients_With_A_Condition_1.sql
@@ -1,5 +1,5 @@
 /*
-Link to the question : https://leetcode.com/problems/patients-with-a-condition/description/
+Link to the question : https://leetcode.com/problems/patients-with-a-condition/
 Leetcode question number : 1527
 */
 select * from patients where conditions regexp '\\bDIAB1'
