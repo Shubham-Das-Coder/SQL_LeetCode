c6a22a19d72ea9e3d24f003c18e08aeb354b1310
Shubham Das <shubhamdaskgp@gmail.com>
2023-10-25 14:41:23 +0530

    Update Not_Boring_Movies_1.sql

--- Not_Boring_Movies_1.sql
+++ Not_Boring_Movies_1.sql
@@ -1,5 +1,5 @@
 /*
-Link to the question : https://leetcode.com/problems/not-boring-movies/description/
+Link to the question : https://leetcode.com/problems/not-boring-movies/
 Leetcode question number : 620
 */
 select * from cinema where mod(id,2)=1 and description<>'boring' order by rating desc;
