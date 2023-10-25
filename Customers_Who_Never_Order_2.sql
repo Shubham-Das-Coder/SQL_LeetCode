690dcd837cf4fb19a969c1a14e0dcc0ed6f311dc
Shubham Das <shubhamdaskgp@gmail.com>
2023-10-25 14:11:29 +0530

    Add files via upload

---
+++ Customers_Who_Never_Order_2.sql
@@ -0,0 +1,13 @@
+/*
+Link to the question : https://leetcode.com/problems/customers-who-never-order/description/
+Leetcode question number : 183
+*/
+select c.name as customers
+from customers c
+where c.id not in
+(
+    select c.id
+    from customers c
+    inner join orders o
+    on c.id=o.customerid
+)
