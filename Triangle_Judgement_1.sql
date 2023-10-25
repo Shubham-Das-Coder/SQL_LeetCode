05a30a537b7b357b757ad9469c6931e00ad14046
Shubham Das <shubhamdaskgp@gmail.com>
2023-10-25 14:40:44 +0530

    Update Triangle_Judgement_1.sql

--- Triangle_Judgement_1.sql
+++ Triangle_Judgement_1.sql
@@ -1,5 +1,5 @@
 /*
-Link to the question : https://leetcode.com/problems/triangle-judgement/description/
+Link to the question : https://leetcode.com/problems/triangle-judgement/
 Leetcode question number : 610
 */
 select *,if(x+y>z and y+z>x and z+x>y,'Yes','No') as triangle from triangle;
