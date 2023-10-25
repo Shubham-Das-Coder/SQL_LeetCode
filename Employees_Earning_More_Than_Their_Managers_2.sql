41b0197e848ca073e5d5d4d3de27354b3ccba46a
Shubham Das <shubhamdaskgp@gmail.com>
2023-10-25 14:42:14 +0530

    Update Employees_Earning_More_Than_Their_Managers_2.sql

--- Employees_Earning_More_Than_Their_Managers_2.sql
+++ Employees_Earning_More_Than_Their_Managers_2.sql
@@ -1,5 +1,5 @@
 /*
-Link to the question : https://leetcode.com/problems/employees-earning-more-than-their-managers/description/
+Link to the question : https://leetcode.com/problems/employees-earning-more-than-their-managers/
 Leetcode question number : 181
 */
 select e1.name employee from employee e1 inner join employee e2 on e1.managerid=e2.id where e1.salary>e2.salary;
