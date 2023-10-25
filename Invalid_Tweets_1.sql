f4030cc273911a75938fd03cd939619acd8e2ed9
Shubham Das <shubhamdaskgp@gmail.com>
2023-10-25 14:41:34 +0530

    Update Invalid_Tweets_1.sql

--- Invalid_Tweets_1.sql
+++ Invalid_Tweets_1.sql
@@ -1,5 +1,5 @@
 /*
-Link to the question : https://leetcode.com/problems/invalid-tweets/description/
+Link to the question : https://leetcode.com/problems/invalid-tweets/
 Leetcode question number : 1683
 */
 select tweet_id from tweets where length(content)>15;
