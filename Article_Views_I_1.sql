5c22cf77e38a994a1c02412cffe09269dfe8e7ba
Shubham Das <shubhamdaskgp@gmail.com>
2023-10-25 14:37:03 +0530

    Update Article_Views_I_1.sql

--- Article_Views_I_1.sql
+++ Article_Views_I_1.sql
@@ -1,5 +1,5 @@
 /*
-Link to the question : https://leetcode.com/problems/article-views-i/description/
+Link to the question : https://leetcode.com/problems/article-views-i/
 Leetcode question number : 1148
 */
 select distinct author_id as id from views where author_id=viewer_id order by id;
