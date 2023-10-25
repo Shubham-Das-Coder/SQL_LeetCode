4d451ac555730712ca748897787afaedbe12010b
Shubham Das <shubhamdaskgp@gmail.com>
2023-10-25 14:40:25 +0530

    Update Duplicate_Emails_2.sql

--- Duplicate_Emails_2.sql
+++ Duplicate_Emails_2.sql
@@ -1,5 +1,5 @@
 /*
-Link to the question : https://leetcode.com/problems/duplicate-emails/description/
+Link to the question : https://leetcode.com/problems/duplicate-emails/
 Leetcode question number : 182
 */
 select email from person group by email having count(distinct id)>1;
